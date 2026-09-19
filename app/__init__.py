0351ca18f1e52930fd6151aa93467473752933a2
secil keser <secilkeser03@gmail.com>
2026-09-19 15:21:04 +0300

    Load app config and initialize database

--- app/__init__.py
+++ app/__init__.py
@@ -1,17 +1,21 @@
-import os
+
 from flask import Flask
 from dotenv import load_dotenv
 
 from app.routes import main
 from app.pages import pages
+from config import config
+from app.database import init_db
 
 load_dotenv()
 
 
 def create_app():
     app = Flask(__name__)
-    app.config["SECRET_KEY"] = os.environ["FLASK_SECRET_KEY"]
+    app.config.from_object(config["development"])
     app.json.ensure_ascii = False
+    with app.app_context():
+        init_db()
 
     app.register_blueprint(main)
     app.register_blueprint(pages)
